466fb7ed6f707b63bd1741e4e09540e2c01b5ae6
Stephan Roloff <sr@werbeagenten.de>
2025-01-20 16:20:07 +0100

    deleting position block

--- src/position-block/save.js
+++
@@ -1,6 +0,0 @@
-import { __ } from '@wordpress/i18n';
-import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
-
-export default function save() {
-    return <InnerBlocks.Content />
-}
