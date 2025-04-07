b638853fbb63d38fb7c68b7fc7540cd680896648
Stephan Roloff <sr@werbeagenten.de>
2025-04-07 12:06:53 +0200

    changing time vertical accordion

--- src/accordion-vertical-block/loader-logic.js
+++ src/accordion-vertical-block/loader-logic.js
@@ -6,5 +6,5 @@ export function accordionVerticalLoaderLogik(element){
         loader.style.display = 'none';
         accordionContainer.style.display = 'flex';
     
-    }, 10000);
+    }, 1000);
 }
