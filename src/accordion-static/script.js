6a53b7647695d28c0b635818fb6aba61c6448c3e
Stephan Roloff <sr@werbeagenten.de>
2025-05-14 15:36:30 +0200

    some important changes

--- src/accordion-static/script.js
+++ src/accordion-static/script.js
@@ -3,15 +3,22 @@ setTimeout(() => {
     let accordions = document.querySelectorAll('.accordion');
 
     accordions.forEach(accordion => {
+        let panel = accordion.nextElementSibling;
+        
         accordion.addEventListener('click', () => {
             accordion.classList.toggle('accordion-active');
-            let panel = accordion.nextElementSibling;
+
             if (panel.style.maxHeight) {
                 panel.style.maxHeight = null;
             } else {
                 panel.style.maxHeight = panel.scrollHeight + "px";
             }
         });
+
+        if(accordion.classList.contains('accordion-active')){
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
     })
+
 
 }, 1000)
