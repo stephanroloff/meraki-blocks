2a874202c66bf4bcb1f3dcfdf1787700dd1f48f5
Stephan Roloff <sr@werbeagenten.de>
2025-06-05 09:55:56 +0200

    important tab change

--- src/tabs-block/frontend.js
+++ src/tabs-block/frontend.js
@@ -1,45 +1,51 @@
 //JS just Frontend
 document.addEventListener("DOMContentLoaded", function() {
-    let allSingleTabs = document.querySelectorAll('.tab-content .single-tab-container');
-    let listTabsContainer = document.querySelector('.list-tabs-titles');
+    let allTabBlocks = document.querySelectorAll('.wp-block-create-block-tabs-block');
 
-    // Inicializar tabs
-    allSingleTabs.forEach((tab, index) => {
-        index == 0 ? tab.classList.add(`active`) : '';
-        tab.classList.add(`tab-${index}`);
+    allTabBlocks.forEach(tabBlock => {
 
-        let singleTabTitleElement = document.createElement('button');
-        singleTabTitleElement.classList.add(`single-tab-title`);
-        singleTabTitleElement.classList.add(`tab-${index}`);
-        singleTabTitleElement.setAttribute('tabindex', '0');  // Agregar tabindex="0"
-        listTabsContainer.appendChild(singleTabTitleElement);
-        singleTabTitleElement.innerHTML = tab.dataset.tabTitle;
-    });
-
-    let allSingleTabTitle = listTabsContainer.querySelectorAll('.single-tab-title');
-
-    // Manejar la activación de las pestañas
-    allSingleTabTitle.forEach((singleTabTitle, index) => {
-        index == 0 ? singleTabTitle.classList.add(`active`) : '';
-
-        singleTabTitle.addEventListener('click', () => {
-            allSingleTabTitle.forEach(t => t.classList.remove("active"));
-            singleTabTitle.classList.add("active");
-
-            let currentlyActive = [...singleTabTitle.classList].find(cls => cls.startsWith('tab-')); 
-            
-            // Primero, ocultar todas las pestañas y luego mostrar la activa
-            allSingleTabs.forEach(tab => {
-                tab.classList.remove("active");
-                tab.scrollTop = 0;  // Resetea el scroll de todas las pestañas cuando se desactiva
-            });
-
-            allSingleTabs.forEach(tab => {
-                if (tab.classList.contains(currentlyActive)) {
-                    tab.classList.add('active');
-                    tab.scrollTop = 0;  // Aseguramos que la pestaña activa comience desde el top
-                }
+        let allSingleTabs = tabBlock.querySelectorAll('.tab-content .single-tab-container');
+        let listTabsContainer = tabBlock.querySelector('.list-tabs-titles');
+    
+        // Inicializar tabs
+        allSingleTabs.forEach((tab, index) => {
+            index == 0 ? tab.classList.add(`active`) : '';
+            tab.classList.add(`tab-${index}`);
+    
+            let singleTabTitleElement = document.createElement('button');
+            singleTabTitleElement.classList.add(`single-tab-title`);
+            singleTabTitleElement.classList.add(`tab-${index}`);
+            singleTabTitleElement.setAttribute('tabindex', '0');  // Agregar tabindex="0"
+            listTabsContainer.appendChild(singleTabTitleElement);
+            singleTabTitleElement.innerHTML = tab.dataset.tabTitle;
+        });
+    
+        let allSingleTabTitle = listTabsContainer.querySelectorAll('.single-tab-title');
+    
+        // Manejar la activación de las pestañas
+        allSingleTabTitle.forEach((singleTabTitle, index) => {
+            index == 0 ? singleTabTitle.classList.add(`active`) : '';
+    
+            singleTabTitle.addEventListener('click', () => {
+                allSingleTabTitle.forEach(t => t.classList.remove("active"));
+                singleTabTitle.classList.add("active");
+    
+                let currentlyActive = [...singleTabTitle.classList].find(cls => cls.startsWith('tab-')); 
+                
+                // Primero, ocultar todas las pestañas y luego mostrar la activa
+                allSingleTabs.forEach(tab => {
+                    tab.classList.remove("active");
+                    tab.scrollTop = 0;  // Resetea el scroll de todas las pestañas cuando se desactiva
+                });
+    
+                allSingleTabs.forEach(tab => {
+                    if (tab.classList.contains(currentlyActive)) {
+                        tab.classList.add('active');
+                        tab.scrollTop = 0;  // Aseguramos que la pestaña activa comience desde el top
+                    }
+                });
             });
         });
-    });
+    })
+    
 });
